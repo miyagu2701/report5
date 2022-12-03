7b903fe3c874806da038a9e36c7063bc1ddad9fa
Miyaguni Mikihiro <e225753@ie.u-ryukyu.ac.jp>
2022-12-04 08:53:00 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
+    
     public static void main(String[] args){
+    try{
         String str = null;
         System.out.println(str.length());
-    }    
+    }catch(NullPointerException e){
+System.out.println("NullPointerExceptionが発生しました。");
+System.out.println(e.getMessage());
+    }} 
 }
